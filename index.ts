dee853d77aeb9cf877f1b88b32ea53a9f7bd8344
MuheebUrRehman <muhiburrehman94@gmail.com>
2024-03-24 17:47:07 +0500

    formatted code

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const answer = await inquirer.prompt([
   {
     name: "userGuessedNumber",
     type: "number",
-    message: "please guess a number between 1-6:",
+    message: "please guess a number between 1-6: ",
   },
 ]);
 
